f2fd3a006450b98a4758e8844c3bb2885da6687f
oragekk <oragekk@163.com>
2023-03-11 23:08:19 +0800

    no message

--- src/.vuepress/env.d.ts
+++ src/.vuepress/env.d.ts
@@ -1,6 +1,6 @@
 declare module "*.vue" {
-    import { DefineComponent } from "vue"
-    const component: DefineComponent<{}, {}, any>
+    import { defineComponent } from "vue"
+    const component: ReturnType<typeof defineComponent>
     export default component
   }
   
