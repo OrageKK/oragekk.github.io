ac401b0f3deb65c18555b34843a059e833c12b79
OrageKK <orageKK@163.com>
2023-03-29 14:04:53 +0000

    Deploying to gh-pages from @ OrageKK/oragekk.github.io@2c1dbd79651c55e396a90bb844b642fbc28df286 🚀

--- assets/pageview-55b8126c.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-bcd4e26f.js";import"./framework-38e902ea.js";const a=()=>e({serverURL:"https://waline-oragekk.vercel.app/"});export{a as updatePageview};
---
+++ assets/pageview-fe205388.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-9f2ef2d2.js";import"./framework-38e902ea.js";const a=()=>e({serverURL:"https://talk.oragekk.me/"});export{a as updatePageview};
