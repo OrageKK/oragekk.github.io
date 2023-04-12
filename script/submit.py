216ae8cb7acbe0abd115a0e99c727ae38f32639a
oragekk <oragekk@163.com>
2023-04-12 16:19:13 +0800

    🍭🚀

--- script/submit.py
+++ script/submit.py
@@ -4,24 +4,28 @@
 import os
 import requests
 import xml.etree.ElementTree as ET
+
 # 提取sitemap中的所有URL
 
 
-def extract_urls_from_sitemap(sitemap_url):
-    response = requests.get(sitemap_url)
-    sitemap_xml = response.content
-    sitemap_root = ET.fromstring(sitemap_xml)
-    urls = []
-    for child in sitemap_root:
-        url = child[0].text
-        urls.append(url)
+def extract_urls_from_sitemap(sitemap_path):
+    with open(sitemap_path, 'r') as f:
+        tree = ET.parse(f)
+        root = tree.getroot()
+        urls = []
+        for url in root.findall('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
+            # 获取 loc 元素的文本内容
+            loc = url.find(
+                '{http://www.sitemaps.org/schemas/sitemap/0.9}loc').text
+            urls.append(loc)
+        print(urls)
     return urls
 
 # 推送URL列表到百度站长
 
 
 def push_urls_to_baidu(urls, site_url):
-    token = os.environ["BAIDU_KEY"]#因为$前面是大写所以也是大写
+    token = os.environ["BAIDU_KEY"]  # 因为$前面是大写所以也是大写
     url = 'http://data.zz.baidu.com/urls?site={}&token={}'.format(
         site_url, token)
     headers = {
@@ -32,14 +36,15 @@ def push_urls_to_baidu(urls, site_url):
     data = '\n'.join(urls)
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == 200:
-        print("百度推送成功:",response.content)
+        print("百度推送成功!:", response.content)
     else:
         print("百度Error ", response.content)
 
+# 调用Bing API提交URL
+
 
 def push_urls_to_bing(urls):
-    # 调用Bing API提交URL
-    bing_api_key = os.environ["BING_KEY"]#因为$前面是大写所以也是大写
+    bing_api_key = os.environ["BING_KEY"]  # 因为$前面是大写所以也是大写
     bing_api_url = "https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlBatch?apikey=" + bing_api_key
 
     headers = {"Content-Type": "application/json"}
@@ -51,14 +56,16 @@ def push_urls_to_bing(urls):
 
     response = requests.post(bing_api_url, json=data, headers=headers)
     if response.status_code == 200:
-        print("Bing 推送成功!:",response.content)
+        print("Bing 推送成功!:", response.content)
     else:
         print("Bing Error ", response.content)
 
+
 # 主程序
 if __name__ == '__main__':
-    sitemap_url = 'https://oragekk.me/sitemap.xml'
+    # sitemap_url = 'https://oragekk.me/sitemap.xml'
+    sitemap_path = os.path.join(os.getcwd(), 'sitemap.xml')
     site_url = 'https://oragekk.me'
-    urls = extract_urls_from_sitemap(sitemap_url)
-    push_urls_to_bing(urls)
-    push_urls_to_baidu(urls, site_url)
+    urls = extract_urls_from_sitemap(sitemap_path)
+    # push_urls_to_bing(urls)
+    # push_urls_to_baidu(urls, site_url)
---
+++ src/private/test.md
@@ -0,0 +1,9 @@
+---
+title: "123"
+date: 2017-01-16
+icon: alias
+tag:
+  - 生活
+---
+
+![Bing 每日壁纸](https://file.mo7.cc/api/public/bz?idx=1)
