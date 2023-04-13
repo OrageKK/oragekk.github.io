32ae473d7dde37726327d4191401a43c0fc44f06
oragekk <oragekk@163.com>
2023-04-13 13:23:59 +0800

    no ci

--- script/submit.py
+++ script/submit.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import json
 import os
 import requests
 import argparse
 import xml.etree.ElementTree as ET
+from oauth2client.service_account import ServiceAccountCredentials
+import httplib2
 
-# 提取sitemap中的所有URL
+# 从命令行参数提取sitemap
 
 
-def getArgs():
+def getSiteMap():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sitemap', help='Path to sitemap file', required=True)
+    parser.add_argument(
+        '--sitemap', help='Path to sitemap file', required=True)
     args = parser.parse_args()
-    print(f"Sitemap path: {args}")
+    print(f"sitemap path: {args.prevsitemap}")
     return args.sitemap
 
+# 提取上次提交的sitemap
+
+
+def getPrevSiteMap():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--prevsitemap', help='Path to prevsitemap file', required=True)
+    args = parser.parse_args()
+    print(f"prevsitemap path: {args.prevsitemap}")
+    return args.prevsitemap
+
+# 从sitemap提取url
+
+
 def extract_urls_from_sitemap(sitemap_path):
+    urls = []
     with open(sitemap_path, 'r') as f:
         tree = ET.parse(f)
         root = tree.getroot()
-        urls = []
         for url in root.findall('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
             # 获取 loc 元素的文本内容
             loc = url.find(
@@ -28,6 +46,13 @@ def extract_urls_from_sitemap(sitemap_path):
             urls.append(loc)
         print(urls)
     return urls
+
+# 获取更新的url
+
+
+def diff_urls(urls, prev_urls):
+    final_urls = list(set(urls) ^ set(prev_urls))
+    return final_urls
 
 # 推送URL列表到百度站长
 
@@ -44,36 +69,69 @@ def push_urls_to_baidu(urls, site_url):
     data = '\n'.join(urls)
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == 200:
-        print("百度推送成功!:", response.content)
+        print(f"🎉百度推送成功!:\n{response.content}")
     else:
-        print("百度Error ", response.content)
+        print(f"🛎百度Error:\n{response.content} ")
 
 # 调用Bing API提交URL
 
 
-def push_urls_to_bing(urls):
+def push_urls_to_bing(urls, site_url):
     bing_api_key = os.environ["BING_KEY"]  # 因为$前面是大写所以也是大写
     bing_api_url = "https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlBatch?apikey=" + bing_api_key
 
     headers = {"Content-Type": "application/json"}
 
     data = {
-        "siteUrl": "https://oragekk.me",
+        "siteUrl": site_url,
         "urlList": urls
     }
 
     response = requests.post(bing_api_url, json=data, headers=headers)
     if response.status_code == 200:
-        print("Bing 推送成功!:", response.content)
+        print(f"🎉Bing推送成功!:\n{response.content}")
     else:
-        print("Bing Error ", response.content)
+        print(f"🛎Bing Error:\n{response.content} ")
+
+# Google index API
+
+
+def push_urls_to_google(urls):
+    SCOPES = ["https://www.googleapis.com/auth/indexing"]
+    ENDPOINT = "https://indexing.googleapis.com/v3/urlNotifications:publish"
+
+    # service_account_file.json is the private key that you created for your service account.
+    JSON_KEY_FILE = os.environ["GOOGLE_JSON"]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        JSON_KEY_FILE, scopes=SCOPES)
+
+    http = credentials.authorize(httplib2.Http())
+
+    content = []
+    for url in urls:
+        item = {
+            "url": url,
+            "type": "URL_UPDATED"
+        },
+        content.append(item)
+    body = json.dumps({"urlNotifications": content}).encode("utf-8")
+    response, content = http.request(ENDPOINT, method="POST", body=body)
+    if response.status_code == 200:
+        print(f"🎉Google推送成功!:\n{response.content}")
+    else:
+        print(f"🛎Google Error:\n{response.content} ")
 
 
 # 主程序
 if __name__ == '__main__':
-    # sitemap_url = 'https://oragekk.me/sitemap.xml'
-    sitemap_path = getArgs()
     site_url = 'https://oragekk.me'
+    sitemap_path = getSiteMap()
+    prev_sitemap_path = getPrevSiteMap()
     urls = extract_urls_from_sitemap(sitemap_path)
-    # push_urls_to_bing(urls)
-    # push_urls_to_baidu(urls, site_url)
+    prev_urls = extract_urls_from_sitemap(prev_sitemap_path)
+    final_urls = diff_urls(urls, prev_urls)
+    print(f"最终url:{final_urls}")
+    push_urls_to_bing(urls, site_url)
+    push_urls_to_baidu(urls, site_url)
+    push_urls_to_google(urls)
---
+++ src/private/test.md
@@ -0,0 +1,10 @@
+---
+title: "测试"
+date: 2017-01-16
+icon: alias
+tag:
+  - 生活
+---
+
+![Bing 每日壁纸](https://file.mo7.cc/api/public/bz?idx=1)
+
