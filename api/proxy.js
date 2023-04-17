6f7cb468a9a911ad52760af9c327694167795dab
oragekk <oragekk@163.com>
2023-04-17 19:11:49 +0800

    🍭user es🚀

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,14 @@
 // 该服务为 vercel serve跨域处理
-import createProxyMiddleware from 'http-proxy-middleware'
-module.exports = (req, res) => {
+import { createProxyMiddleware } from 'http-proxy-middleware'
+export default (req, res) => {
   let target = ''
   if (req.url.startsWith('/bing')) { 
-    target = 'https://cn.bing.com' //这里就是在vite中配置的一样
+    target = 'https://cn.bing.com'
   }
-  // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
       '^/bing/': '/'
     }
   })(req, res)
