867074689030fea61d501289d8aca564caee4428
oragekk <oragekk@163.com>
2023-08-15 20:00:03 +0800

    🍭update paypal link🚀

--- src/.vuepress/theme/components/Sponsor.vue
+++ src/.vuepress/theme/components/Sponsor.vue
@@ -34,7 +34,7 @@
 import { ref, reactive, computed, onMounted } from "vue";
 import { useWindowSize } from "@theme-hope/composables/index";
 const githubLink = "https://github.com/OrageKK/sponsor-page";
-const paypalLink = "https://www.paypal.me/KaiyuanXie";
+const paypalLink = "https://www.paypal.com/paypalme/oragekk";
 const qrCodes = reactive({
   // btc_donate: "/assets/images/BTCQR.png",
   alipay_donate: "/assets/images/AliPayQR.jpg",
