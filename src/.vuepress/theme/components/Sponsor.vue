da288ceb5b24f52e8038b3d700d7d268c5c41e7d
oragekk <oragekk@163.com>
2023-08-15 20:12:46 +0800

    🍭🚀

--- src/.vuepress/theme/components/Sponsor.vue
+++ src/.vuepress/theme/components/Sponsor.vue
@@ -34,7 +34,7 @@
 import { ref, reactive, computed, onMounted } from "vue";
 import { useWindowSize } from "@theme-hope/composables/index";
 const githubLink = "https://github.com/OrageKK/sponsor-page";
-const paypalLink = "https://www.paypal.com/paypalme/oragekk";
+const paypalLink = "https://www.paypal.me/oragekk";
 const qrCodes = reactive({
   // btc_donate: "/assets/images/BTCQR.png",
   alipay_donate: "/assets/images/AliPayQR.jpg",
