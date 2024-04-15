b26f5076189a29eb1764e357025c1879940481a7
oragekk <oragekk@163.com>
2024-04-15 17:00:41 +0800

    recaptcha

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -140,6 +140,7 @@ export default MyTheme({
       reaction: true,
       requiredMeta: ["nick",'mail'],
       wordLimit: 300,
+      recaptchaV3Key:"6LcI0bopAAAAAE7PyKIZ7HnMP8Dro6JiHlX3E4CB",
       emoji: [
         "https://unpkg.com/@waline/emojis@1.1.0/tieba",
         "https://unpkg.com/@waline/emojis@1.1.0/weibo",
