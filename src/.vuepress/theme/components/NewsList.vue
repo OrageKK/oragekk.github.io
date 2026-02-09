b03fa6c5e90dcb31042bb744ece9555e23cdd480
oragekk <oragekk@163.com>
2026-02-09 15:49:48 +0800

    🍭update img link🚀

--- src/.vuepress/theme/components/NewsList.vue
+++ src/.vuepress/theme/components/NewsList.vue
@@ -5,7 +5,7 @@
       共{{ items.length }}条碎碎念~ (｡♥‿♥｡)
     </p>
     <figure>
-      <img class="news-top-img" src="https://tuapi.eees.cc/api.php?category=dongman&px=pc&type=302" alt="图"
+      <img class="news-top-img" src="http://api.btstu.cn/sjbz/?lx=suiji" alt="图"
         :key="imageKey" />
     </figure>
     <template v-if="currentArticles.length">
